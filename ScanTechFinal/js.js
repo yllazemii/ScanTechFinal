41cecc727bef08a86e4436fc67844e5e872cb7f6
yllazemii <yllazemi41@gmail.com>
2025-11-13 07:51:00 -0800

    also added a carousel

--- ScanTechFinal/js.js
+++ ScanTechFinal/js.js
@@ -22,3 +22,40 @@ function updateClockStatus() {
 // Initial call and update every second
 updateClockStatus();
 setInterval(updateClockStatus, 1000);
+
+let slideIndex = 0;
+showSlides();
+
+// Automatically show slides
+function showSlides() {
+  let slides = document.getElementsByClassName("carousel-slide");
+  let dots = document.getElementsByClassName("dot");
+  
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1; }
+  
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  
+  setTimeout(showSlides, 4000); // change slide every 4 seconds
+}
+
+// Manual navigation with arrows
+function changeSlide(n) {
+  slideIndex += n - 1; // because showSlides auto increments
+  showSlides();
+}
+
+// Click on dot to jump to slide
+function currentSlide(n) {
+  slideIndex = n - 1;
+  showSlides();
+}
